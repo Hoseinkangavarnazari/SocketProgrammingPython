fd2ff1c1884d7c900127f4a70ad7b30f46cdf41f
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-05-07 14:37:01 +0430

    in UDP a client can send multiple msgs now

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 import sys
 
-serverAddr = 'localhost'
+serverAddrConst = 'localhost'
 serverPort = 4000
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     msg = input('input your sentence: ')
-    clientSocket.sendto(msg.encode(), (serverAddr, serverPort))
+    clientSocket.sendto(msg.encode(), (serverAddrConst, serverPort))
     modifiedSentence, serverAddr = clientSocket.recvfrom(1024)
     print("Modified sentence received from server : ", modifiedSentence.decode())

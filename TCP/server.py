19d1560381d51a8a51d2c363e74663d98c892234
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-05-07 17:55:32 +0430

    Adding client and server TCP Socket

---
+++ TCP/server.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 5000
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((SERVER_HOST, SERVER_PORT))
+
+serverSocket.listen(2)
+print("Server is ready ... on port ", SERVER_PORT)
+connectionSocket, addr = serverSocket.accept()
+
+while True:
+    data = connectionSocket.recv(1024)
+    modifiedData = str(data.decode()).upper()
+    print("Message: ", data.decode())
+    connectionSocket.send(modifiedData.encode())
+
+connectionSocket.close()
+

b1c1a5b02cdfa442d18e4dfc96b03e4532997833
Hosein Kangavar Nazari <hkangavarnazari@gmail.com>
2019-06-02 11:43:10 +0430

    adding SMTP client

---
+++ SMTP_CLIENT/index.py
@@ -0,0 +1,23 @@
+import smtplib
+
+gmail_user = 'hkangavarnazari@gmail.com'  
+gmail_password = '33739593383716Nn'
+
+
+sent_from = gmail_user  
+to = 'amirhosein.simoorgh@gmail.com'
+subject = 'OMG Super Important Message'  
+body = 'Test mail for SMTP '
+
+email_text = " this is just for test"
+
+try:  
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print ('Email sent!')
+except:  
+    print ('Something went wrong...')
